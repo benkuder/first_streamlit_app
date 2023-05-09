80a48676a8303957e6008eb3cbdfc7536cda1a38
benkuder <132941230+benkuder@users.noreply.github.com>
2023-05-09 15:20:46 -0500

    Update streamlit_app.py
    fixing add record to snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,4 +64,6 @@ def insert_row_snowflake(new_fruit):
          return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 streamlit.write(insert_row_snowflake(add_my_fruit))
+my_cnx.close()
