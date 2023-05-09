09556e88e32ca0bbe8258b41d633eb306da1c16a
benkuder <132941230+benkuder@users.noreply.github.com>
2023-05-09 15:56:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,8 +72,8 @@ try:
   else:
        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
        streamlit.write(insert_row_snowflake(add_my_fruit))
-       add_my_fruit = ''
        my_cnx.close()
+  add_my_fruit = ''
 except URLError as e:
     streamlit.error()
 
