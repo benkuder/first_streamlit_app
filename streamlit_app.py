dd4accc5657413122108d96912c9cefc64b0841f
benkuder <132941230+benkuder@users.noreply.github.com>
2023-05-10 09:14:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ try:
   add_my_fruit = streamlit.text_input('What fruit would you like to add?',[])
   if not add_my_fruit:
        streamlit.error("Please enter a fruit you would like to add.")
-  elif add_my_fruit == '':
+  elif add_my_fruit == []:
        streamlit.error("Please enter a fruit you would like to add.")        
   else:
        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
